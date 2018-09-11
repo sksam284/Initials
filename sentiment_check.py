e73005179b41b86b1a4790bdab9f804a2925cd4d
sksam284 <er.sksumit@gmail.com>
2018-09-11 12:40:55 +0530

    Update sentiment_check.py

--- sentiment_check.py
+++ sentiment_check.py
@@ -1,4 +1,11 @@
 from textblob import TextBlob
 text = raw_input("Enter the tweet:")
 print "nearer to 1 means a positive sentiment and values nearer to -1 means a negative sentiment"
-print "sentiment of tweet is:",TextBlob(text).sentiment[0]
+sentiment = TextBlob(text).sentiment[0]
+if sentiment==0:
+    mode = "neutral"
+elif sentiment<0:
+    mode = "sad"
+elif sentiment>0:
+    mode = "happy"
+print "sentiment of tweet is:%s , with order: %s"%(mode, sentiment)
